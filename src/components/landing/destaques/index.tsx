05a47e0710445dc727ebeb4a5b132103222e4c34
tiago-kelis <tiagokelis2017@gmail.com>
2025-03-13 01:04:39 -0300

    adicionando imagens e implementando destaques

--- src/components/landing/destaques/index.tsx
+++ src/components/landing/destaques/index.tsx
@@ -1,11 +1,15 @@
 import Area from "../comuns/Area";
 import Slogan from "./Slogan";
+import Principal from "../../../../public/segundo.jpg"
+import ImagemResponsiva from "../comuns/ImagemResponsiva";
 
 export default function Destaques() {
     return (
-       <Area id="inicio" className="pt-32">
-          <div className={`h-[500px]`}>
+       <Area id="inicio" className="pt-28">
+     
+         <div className="flex items-center justify-around h-[500px]">
             <Slogan/>
+            <ImagemResponsiva imagem={Principal} className="rotate-3 hidden md:inline w-48 h-48"/>
           </div>          
        </Area>
     )
