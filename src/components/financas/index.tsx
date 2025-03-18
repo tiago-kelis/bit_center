e463fed0dc667390ec3d6418a439ce702954e215
tiago-kelis <tiagokelis2017@gmail.com>
2025-03-18 16:33:53 -0300

    implemetando a funcao excluir

--- src/components/financas/index.tsx
+++ src/components/financas/index.tsx
@@ -7,6 +7,7 @@ import Lista from "./Lista";
 import Transacao from "@/logic/core/financas/Transacao";
 import  TransacoesFalsas  from "@/data/constants/TransacoesFalsas";
 import Formulario from "./Formulario";
+import NaoEncontrado from "../templates/NaoEncontrado";
 
 
 
@@ -15,22 +16,39 @@ export default function Financas() {
    const [transacoes, setTransacoes ] = useState<Transacao[]>(TransacoesFalsas);
    const [transacao, setTransacao ] = useState<Transacao | null>(null);  
 
-    return (
-       <Pagina>
+      function salvar(transacao: Transacao){
+       
+      }
+
+      function excluir(transacao: Transacao){
+         console.log("excluir", transacao)
+         const outrasTransacoes = transacoes.filter(t => t.id !== transacao.id)
+         setTransacoes(outrasTransacoes)
+         setTransacao(null)
+      }
+
+   return (
+      <Pagina>
          <Cabecalho/>
          <Conteudo className="gap-5">
             
             {transacao ? (
                <Formulario
-                transacao={transacao}
-                cancelar={() => setTransacao(null)}
+               transacao={transacao}
+               salvar={salvar}
+               excluir={excluir}
+               cancelar={() => setTransacao(null)}
                />
                
-            ):(
+            ): transacoes.length ? (
                <Lista transacoes={transacoes} selecionarTransacao={setTransacao}/>
+            ): (
+               <NaoEncontrado>
+                  Nenhuma transação encontrada
+               </NaoEncontrado>
             )}
                         
          </Conteudo>
-       </Pagina>
-    )
+      </Pagina>
+   )
 }
