c04c8bab207be0d7c198c4537dc741f60417d43c
tiago-kelis <tiagokelis2017@gmail.com>
2025-05-15 16:07:19 -0300

    modified:   src/components/templates/Cabecalho.tsx

--- src/components/templates/Cabecalho.tsx
+++ src/components/templates/Cabecalho.tsx
@@ -4,7 +4,7 @@ import MenuUsuario from "./MenuUsuario";
 
 export default function Cabecalho() {
     return (
-       <div className="flex justify-between items-center p-5 border-b border-zinc-800 bg-black">
+       <div className="flex justify-between items-center p-5 border-b border-zinc-800 bg-zinc-900">
          <BoaVindas/>
          <MenuUsuario titulo_1="finanças" titulo_2="Meus Dados" titulo_3="Sair"/>     
        </div>   
